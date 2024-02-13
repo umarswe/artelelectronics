9218791073a0efc6dc7d558cf69de0427b946fb5
umaar-kr <umaar.kr@gmail.com>
2024-02-13 22:04:32 +0500

    update

--- src/sections/AboutUs/Hero/index.jsx
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-
-function Hero() {
-  return <div className="about-us__hero"></div>;
-}
-
-export default Hero;
--- src/sections/Home/index.jsx
+++
@@ -1,7 +0,0 @@
-import Statistics from "./Statistics";
-import ProductInfo from "./ProductInfo";
-import RecommendedProducts from "./RecommendedProducts";
-import NewProducts from "./NewProducts";
-import Services from "./Services";
-
-export { Statistics, ProductInfo, RecommendedProducts, NewProducts, Services };
